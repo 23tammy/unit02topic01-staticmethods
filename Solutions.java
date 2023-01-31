d6e246b75b1b92611012d773c31d63d1a7751bf2
tammy vu <tammy.vsk@gmail.com>
2023-01-31 12:23:42 -0600

    stuc

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,45 @@
 public class Solutions {
-    public static void main(String[] args) {
+
+    public static int max3(int a, int b, int c){
+        if (a > b && a > c){
+            return a;
+        }
+        if (b > a && b > c){
+            return b;
+        }
+        if (c > a && c > b){
+            return c;
+        }
+        return 0;
+    }
+    public static double max3(double a, double b, double c) {
+        if (a > b && a > c){
+            return a;
+        }
+        if (b > a && b > c){
+            return b;
+        }
+        if (c > a && c > b){
+            return c;
+        }
+        return 0;
         
+    }
+    public static boolean odd(boolean a, boolean b, boolean c) {
+        if (a && !b && !c || !a && b && !c || !a && !b && c){
+            return true;
+        }if (!a || !b || !c){
+            return false;    
+        }else{
+            return true;
+        }
+        
+    }
+    public static boolean majority(boolean a, boolean b, boolean c){
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println(odd(false, false, false));
     }
 }
